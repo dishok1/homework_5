0a453b9d1ade3817df07e3c5c6240f26fa183690
Dima Panu <149871287+dishok1@users.noreply.github.com>
2025-11-24 16:16:38 +0200

    Add homework_6.js
    update index.html

---
+++ homework_5.js
@@ -0,0 +1,28 @@
+// створити телефонний довідник
+
+// створити початковий клас Abonent, де зберігатимуться ім*я і номер
+// створити set який прийматиме телефон і номер
+// створити get який виводитиме данні про абонента
+// створити три різних юзери
+// вивести данні про юзерів в консоль
+
+// class Abonent {
+//   constructor(name, number) {
+//     this.name = name;
+//     this.number = number;
+//   }
+
+//   set phone(number) {
+//     this.number = number;
+//   }
+
+//   get info() {
+//     return `Name: ${this.name}, Phone Number: ${this.number}`;
+//   }
+// }
+// const Olesiya = new Abonent("Olesiya", "380(67)123-45-67");
+// const Olena = new Abonent("Olena", "380(63)987-65-43");
+// const Martin = new Abonent("Martin", "380(50)555-55-55");
+// console.log(Olesiya.info); // name: Olesiya, Phone Number: 380(67)123-45-67
+// console.log(Olena.info); // Name: Olena, Phone Number: 380(63)987-65-43
+// console.log(Martin); // Abonent {name: 'Martin', number: '380(50)555-55-55'}
